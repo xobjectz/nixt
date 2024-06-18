b83419763730d052dc2b59ececf4dba29cca63b2
Bart Thate <bthate@dds.nl>
2024-06-18 06:38:15 +0200

    add tests

---
+++ tests/test_none.py
@@ -0,0 +1,16 @@
+# This file is placed in the Public Domain.
+#
+#
+
+
+"no tests"
+
+
+import unittest
+
+
+
+class TestNone(unittest.TestCase):
+
+    def test_nope(self):
+        self.assertTrue(True)
