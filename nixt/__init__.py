5d2614a9e30a7a9df8087f695b71ab6cef0896b5
Bart Thate <bthate@dds.nl>
2024-06-19 15:24:57 +0200

    add nixt init

---
+++ nixt/__init__.py
@@ -0,0 +1,16 @@
+# This file is placed in the Public Domain.
+
+
+"you have been nixt."
+
+
+from . import lib, run, mod, srv
+
+
+def __dir__():
+    return (
+        'lib',
+        'run',
+        'mod',
+        'srv'
+    )
