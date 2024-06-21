bca5bd769ef03535a3f27f6289bc67e02c89f64d
Bart Thate <bthate@dds.nl>
2024-06-21 16:52:25 +0200

    add getmod function

--- nixt/__init__.py
+++ nixt/__init__.py
@@ -2,3 +2,15 @@
 
 
 "you have been nixt."
+
+
+from . import mod, usr
+
+
+def getmod(name):
+    print(mod)
+    print(usr)
+    mods = getattr(mod, name, None)
+    if not mods:
+        mods = getattr(usr, name, None)
+    return mods
