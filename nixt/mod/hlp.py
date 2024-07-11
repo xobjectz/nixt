82d190d2d453675800f70081bca5bfaf89a68e30
Bart Thate <bthate@dds.nl>
2024-07-11 07:44:35 +0200

    add hlp command

---
+++ nixt/mod/hlp.py
@@ -0,0 +1,30 @@
+# This file is placed in the Public Domain.
+
+
+"help"
+
+
+SEP = "/"
+NAME = __file__.split(SEP)[-3]
+TXT = f"""{NAME.upper()}
+
+    {NAME}  <cmd> [key=val] [key==val]
+    {NAME}d
+    {NAME}sh
+
+OPTIONS
+
+    -h     show help
+    -i     start services
+    -v     use verbose
+
+COMMANDS
+    
+    $ {NAME} cmd
+    cfg,cmd,dpl,err,exp,imp,mod,mre,nme,pwd,rem,res,rss,thr
+
+"""
+
+
+def hlp(event):
+    event.reply(TXT)
