7239fe5091602b054f8090698a271057c00274a0
Bart Thate <bthate@dds.nl>
2024-07-14 05:43:41 +0200

    add binaries

--- nixt/__main__.py
+++ nixt/__main__.py
@@ -5,17 +5,15 @@
 "main"
 
 
-import getpass
 import os
-import pwd
 import sys
 
 
 from .cfg   import Config
-from .disk  import Persist, skel, pidfile
-from .main  import cmnd, enable, init, scan
+from .disk  import Persist
+from .main  import cmnd, enable, scan
 from .parse import parse
-from .utils import forever, modnames
+from .utils import modnames
 
 
 from . import modules
@@ -24,53 +22,11 @@ from . import user
 
 Cfg         = Config()
 Cfg.name    = "nixt"
-Cfg.user    = getpass.getuser()
 Cfg.wdr     = os.path.expanduser(f"~/.{Cfg.name}")
 Cfg.pidfile = os.path.join(Cfg.wdr, f"{Cfg.name}.pid")
 
 
 Persist.workdir = Cfg.wdr
-
-
-def daemon(verbose=False):
-    "switch to background."
-    pid = os.fork()
-    if pid != 0:
-        os._exit(0)
-    os.setsid()
-    pid2 = os.fork()
-    if pid2 != 0:
-        os._exit(0)
-    if not verbose:
-        with open('/dev/null', 'r', encoding="utf-8") as sis:
-            os.dup2(sis.fileno(), sys.stdin.fileno())
-        with open('/dev/null', 'a+', encoding="utf-8") as sos:
-            os.dup2(sos.fileno(), sys.stdout.fileno())
-        with open('/dev/null', 'a+', encoding="utf-8") as ses:
-            os.dup2(ses.fileno(), sys.stderr.fileno())
-    os.umask(0)
-    os.chdir("/")
-
-
-def privileges(username):
-    "drop privileges."
-    pwnam = pwd.getpwnam(username)
-    os.setgid(pwnam.pw_gid)
-    os.setuid(pwnam.pw_uid)
-
-
-def background():
-    "main"
-    daemon("-v" in sys.argv)
-    skel()
-    pidfile(Cfg.pidfile)
-    privileges(Cfg.user)
-    if "-v" in sys.argv:
-        enable(print)
-    Cfg.mod = ",".join(modnames(modules, user))
-    scan(Cfg.mod, modules, user)
-    init(Cfg.mod, modules, user)
-    forever()
 
 
 def main():
